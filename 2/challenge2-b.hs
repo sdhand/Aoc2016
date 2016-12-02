4fd6cb63c2d6b976767b8d95b26e3c8864b12bd9
Samuel Hand <samuel.hand@openmailbox.org>
2016-12-02 18:47:25 +0000

    Code for part B can now solve both parts

--- 2/challenge2-b.hs
+++ 2/challenge2-b.hs
@@ -1,6 +1,10 @@
 import qualified Data.Map as Map
 
-keypad = [ [Nothing,  Nothing,  Just '1', Nothing,  Nothing ],
+keypad1 = [ [Just '1', Just '2', Just '3'],
+            [Just '4', Just '5', Just '6'],
+            [Just '7', Just '8', Just '9'] ]
+
+keypad2 = [ [Nothing,  Nothing,  Just '1', Nothing,  Nothing ],
            [Nothing,  Just '2', Just '3', Just '4', Nothing ],
            [Just '5', Just '6', Just '7', Just '8', Just '9'],
            [Nothing,  Just 'A', Just 'B', Just 'C', Nothing ],
@@ -12,20 +16,24 @@ rowMap (x:xs) row column y = rowMap xs row (column+1) $ Map.insert (column, row)
 buildKeypadMap [] _ y = y
 buildKeypadMap (x:xs) row y = buildKeypadMap xs (row-1) $ rowMap x row 0 y
 
-keypadMap = buildKeypadMap keypad 0 Map.empty
+keypadMap2 = buildKeypadMap keypad2 0 Map.empty
+keypadMap1 = buildKeypadMap keypad1 0 Map.empty
 
 move (a, b) 'U' = (a, b+1)
 move (a, b) 'D' = (a, b-1)
 move (a, b) 'R' = (a+1, b)
 move (a, b) 'L' = (a-1, b)
 
-code :: ((Int, Int), Char) -> Char -> ((Int, Int), Char)
-code (x,y) z = case nextCode of
+code keypadMap (x,y) z = case nextCode of
         (_, Nothing) -> (x, y)
         (a, Just b) -> (a, b)
     where result = move x z
           nextCode = if Map.notMember result keypadMap then (x, keypadMap Map.! x) else (result, keypadMap Map.! result)
 
+solveKeypad :: Map.Map (Int, Int) (Maybe Char) -> String -> ((Int, Int), Char) -> String
+solveKeypad keypad input start = map (snd.(foldl (code keypad) start)) (lines input)
+
 main = do
     input <- readFile "input.txt"
-    putStrLn $ map (snd.(foldl code ((0, -2), '5'))) (lines input)
+    putStrLn $ solveKeypad keypadMap2 input ((0, -2), '5')
+    putStrLn $ solveKeypad keypadMap1 input ((1, -1), '5')
