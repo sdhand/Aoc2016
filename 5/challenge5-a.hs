93ce72ebfaff29813330306e508f32fa3a5c1007
Samuel Hand <samuel.hand@openmailbox.org>
2016-12-05 09:49:11 +0000

    Clean up part a a bit

--- 5/challenge5-a.hs
+++ 5/challenge5-a.hs
@@ -1,7 +1,8 @@
 import Data.Digest.Pure.MD5
 import Data.ByteString.Lazy.Char8 (pack)
+import Data.List
 
-password id index = if take 5 md5hash == "00000" then [md5hash !! 5]++(password id (index+1)) else password id (index+1)
+password id index = if isPrefixOf "00000" md5hash then [md5hash !! 5]++(password id (index+1)) else password id (index+1)
     where md5hash = show $ md5 $ pack $ id++(show index)
 
 main = do
