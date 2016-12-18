48b80ecb23aef66b40ecfc1a5471d299dab703c8
Samuel Hand <samuel.hand@openmailbox.org>
2016-12-18 15:11:08 +0000

    Challenge 18 solved

---
+++ 18/challenge18-a.hs
@@ -0,0 +1,18 @@
+data Tile = Safe | Trap deriving (Eq, Show)
+
+makeTile :: Char -> Tile
+makeTile '.' = Safe
+makeTile '^' = Trap
+
+nextRow :: Bool -> [Tile] -> [Tile]
+nextRow first (x:y:z:xs) = (if x' /= z' then Trap else Safe):(nextRow False (y':z':xs'))
+    where x' = if first then Safe else x
+          y' = if first then x else y
+          z' = if first then y else z
+          xs' = if first then (z:xs) else xs
+nextRow _ (x:_:[]) = [x]
+
+main :: IO ()
+main = do
+    input <- readFile "input.txt"
+    print $ length $ filter (\x -> x == Safe) $ concat $ take 40 $ iterate (nextRow True) $ map makeTile $ init input
